7e1e0cc56e9caba7496011ef85aa2f0814dc5722
merabza <merabza@gmail.com>
2026-05-14 14:54:13 +0400

    202605141453

--- SupportTools/DependencyInjection/SupportToolsServices.cs
+++ SupportTools/DependencyInjection/SupportToolsServices.cs
@@ -17,6 +17,7 @@ using SupportTools.Menu.ProjectGroupsList;
 using SupportTools.Menu.SupportToolsParametersEdit;
 using SupportToolsData.Models;
 using SystemTools.BackgroundTasks;
+using SystemTools.SerilogStuff.DependencyInjection;
 using SystemTools.SystemToolsShared.DependencyInjection;
 
 namespace SupportTools.DependencyInjection;
